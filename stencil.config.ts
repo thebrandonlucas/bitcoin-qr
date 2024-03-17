27cf9755bced6b6e36a1644493d5147385cd5dfb
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-03-17 12:26:40 -0400

    try to build without other targets & bump version

--- stencil.config.ts
+++ stencil.config.ts
@@ -7,16 +7,6 @@ export const config: Config = {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
   ],
   testing: {
     browserHeadless: 'new',
