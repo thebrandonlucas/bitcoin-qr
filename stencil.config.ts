e8dc0d73b9a1256354dd6328049deb4db607754f
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-03-17 15:26:14 -0400

    remove www loader for now

--- stencil.config.ts
+++ stencil.config.ts
@@ -8,10 +8,10 @@ export const config: Config = {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
+    // {
+    //   type: 'www',
+    //   serviceWorker: null, // disable service workers
+    // },
   ],
   testing: {
     browserHeadless: 'new',
