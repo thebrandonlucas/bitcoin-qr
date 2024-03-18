c0cf1e462156de623a02a9499d8c19f56bda2725
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-03-18 19:42:39 -0400

    wip add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+/** @type { import('@storybook/html-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/html-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
