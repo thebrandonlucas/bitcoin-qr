5e20b885a48d2705c64e346c8bfcab494175752f
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-04-18 23:07:36 -0400

    add backgrounds to demo site

--- examples/sveltekit-kitchen-sink/src/routes/+layout.svelte
+++ examples/sveltekit-kitchen-sink/src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
 	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
+	
 </script>
 
-<slot />
+<Navbar>
+	<NavBrand href="/">
+	  <img src="/assets/bitcoin.svg" class="me-3 h-6 sm:h-9" alt="Bitcoin Logo" />
+	  <span class="self-center whitespace-nowrap text-xl font-semibold">bitcoin-qr</span>
+	</NavBrand>
+	<NavHamburger  />
+	<NavUl >
+		<NavLi href="/" >playground</NavLi>
+		<NavLi href="/gallery" >gallery</NavLi>
+		<NavLi href="https://github.com/thebrandonlucas/bitcoin-qr" target="_blank">github</NavLi>
+		<NavLi href="https://www.npmjs.com/package/bitcoin-qr" target="_blank" >npm</NavLi>
+	</NavUl>
+  </Navbar>
+
+
+
+  <div >
+	<slot />
+
+  </div>
